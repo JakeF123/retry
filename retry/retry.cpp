ec35ead031b65b267c2a91595a00cd52822d8cbe
alan <alan@LT-1-220706-109>
2022-08-11 10:33:05 +0100

    Add project files.

---
+++ retry/retry.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    int a = 10;
+    int b = 20;
+    int c = a + b;
+    std::cout << c << "\n";
+    int numbers[5]{ 1,2,3,4,5 };
+    {
+        
+    }
+}
